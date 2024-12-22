d9f10c4fe794a05610fcc1360fc25b27957fe846
Fotis Tsakiroglou <tsakirogf@hotmail.com>
2024-12-22 14:05:21 +0200

    Add boilerplate code
      o Add boilerplate code generated with spring initialiser

---
+++ src/main/java/com/fleetmanagement/fleetmanagementapi/FleetmanagementapiApplication.java
@@ -0,0 +1,13 @@
+package com.fleetmanagement.fleetmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetmanagementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetmanagementapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleetmanagement/fleetmanagementapi/FleetmanagementapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetmanagement.fleetmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetmanagementapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
