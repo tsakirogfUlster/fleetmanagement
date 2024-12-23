875423f877c6252a9f97f5ff8518e0b32d347d02
Fotis Tsakiroglou <tsakirogf@hotmail.com>
2024-12-23 13:24:04 +0200

    Add docker compose yaml files

---
+++ heartbeater/src/main/java/com/fleetmanagement/heartbeater/HeartbeaterApplication.java
@@ -0,0 +1,13 @@
+package com.fleetmanagement.heartbeater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeartbeaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeartbeaterApplication.class, args);
+	}
+
+}
---
+++ heartbeater/src/test/java/com/fleetmanagement/heartbeater/HeartbeaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetmanagement.heartbeater;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeartbeaterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
