0d589013f849adc52c96fea6893ca7f11a4535d7
Fotis Tsakiroglou <tsakirogf@hotmail.com>
2024-12-23 20:31:53 +0200

    Add penaltyCalculator

---
+++ penaltycalculator/src/main/java/com/fleetmanagement/penaltycalculator/PenaltycalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.fleetmanagement.penaltycalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenaltycalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenaltycalculatorApplication.class, args);
+	}
+
+}
---
+++ penaltycalculator/src/test/java/com/fleetmanagement/penaltycalculator/PenaltycalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetmanagement.penaltycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenaltycalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
